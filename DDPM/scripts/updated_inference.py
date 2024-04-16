dfce67e2f7561875d45030bc3a52595ba5d98656
Sruthi Srinivasan <63657533+sruthi0107@users.noreply.github.com>
2024-04-16 15:42:05 -0400

    Updated class Diffusion with minor changes

--- DDPM/scripts/updated_inference.py
+++ DDPM/scripts/updated_inference.py
@@ -34,15 +34,17 @@ class Diffusion:
 		self.noise_steps = noise_steps
 		self.beta_start = beta_start
 		self.beta_end = beta_end
-		self.version = version
+
 		self.beta = self.prepare_noise_schedule().to(device)
 		self.alpha = 1. - self.beta
 		self.alpha_hat = torch.cumprod(self.alpha, dim=0)
-
+  
+		self.version = version
+		
 		self.img_size = img_size
-		if self.version==1:
+		if version==1:
 			self.model = UNet_conditional_student_v1(c_in, c_out, num_classes=num_classes,**kwargs).to(device)
-		elif self.version==2:
+		elif version==2:
 			self.model = UNet_conditional_student_v2(c_in, c_out, num_classes=num_classes,**kwargs).to(device)
 		self.ema_model = copy.deepcopy(self.model).eval().requires_grad_(False)
 		self.device = device
@@ -65,60 +67,58 @@ class Diffusion:
 		Ɛ = torch.randn_like(x)
 		return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * Ɛ, Ɛ
 
-	
+
 	#new pruning function - modified
-	def pruning(model):
+	def pruning(self, model):
 
 		mod = copy.deepcopy(model).to(device)
 		l1_prune = torch.nn.utils.prune.L1Unstructured(0.3)
-	
+
 		for name,module in mod.named_modules():
-	
+
 			if isinstance(module,nn.Linear):
 				l1_prune.apply(module,name='weight',amount=0.3)
-	
+
 			elif isinstance(module,nn.Conv2d):
 				l1_prune.apply(module,name='weight',amount=0.3)
-	
+
 		return mod
 
-	
+
 	def find_weights(self,weights,bits):
-	
+
 		min_wt, max_wt = weights.min(), weights.max()
 		qmin, qmax = -2.0 ** (bits - 1), 2.0 ** (bits - 1) - 1
-		
+
 		scale = (max_wt - min_wt) / (qmax - qmin)
-		
+
 		zero_point = qmin - min_wt / scale
-		
+
 		quantized_weights = torch.round(weights / scale + zero_point)
 		quantized_weights = torch.clamp(quantized_weights, qmin, qmax).int()
-		
+
 		return quantized_weights
-	
+
 	#new manual quantization function
 	def quantize_eval(self,model):
-		
+
 		for param_name, param in model.named_parameters():
-			
-			if 'weight' in param_name:  
+
+			if 'weight' in param_name:
 				new_name = param_name.replace(".", "_")
 				quantized_weights = self.find_weights(param.data, bits=8)
 				setattr(model, new_name, param)
-		
+
 		return model
 
 
 	@torch.inference_mode()
 	def sample(self, model, labels, cfg_scale=0):
-		
-  		#model = self.ema_model if use_ema else self.model
 		n = len(labels)
 		logging.info(f"Sampling {n} new images....")
 		model.eval()
 		model = self.quantize_eval(model)
-		#model = self.prune(model)
+		model = self.pruning(model)
 
 
 		with torch.inference_mode():
@@ -141,33 +141,47 @@ class Diffusion:
 		x = (x * 255).type(torch.uint8)
 		return x
 
+	#new plot images function integrated with image filters
+	def plot_images(self,images):
+
+		ilist = []
+		filter_instance = ImageFilter(filter_type='weighted', weight = [0.25, 0.25, 0.25, 0.25])
+
+		for i in images.cpu():
+			image = cv2.cvtColor(i.permute(1,2,0).numpy(),cv2.COLOR_RGBA2RGB)
+			filtered_image = filter_instance.apply_filter(image)
+			ilist.append(filtered_image)
+
+		ilist = torch.Tensor(np.array(ilist))
+		return ilist
+
 	#new method to analyze time
 	def analyze_time(self, model, inp, t, y):
 		with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
 			with record_function("model_inference"):
 				model(x=inp,t=t,y=y)
 		print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
-		
 
-	#new plot images function integrated with image filters
-	def plot_images(self,images):
-		
-		ilist = []
-		filter_instance = ImageFilter(filter_type='weighted', weight = [0, 0, 0.5, 0.5])
-		
-		for i in images.cpu():
-			
-			image = cv2.cvtColor(i.permute(1,2,0).numpy(),cv2.COLOR_RGBA2RGB)
-			filtered_image = filter_instance.apply_filter(image)
-			ilist.append(filtered_image)
-		
-		ilist = torch.Tensor(np.array(ilist))
-		
-		#plt.imshow(ilist[0])
-		#plt.show()
-		#out = ilist.permute(1, 2, 0).cpu()
-		
-		return ilist
+	def count_parameters(self, model):
+		return sum(p.numel() for p in model.parameters())
+
+	def count_flops(self, model, inputs, t, y):
+		flops = FlopCountAnalysis(model, (inputs, t, y))
+		return flops.total()
+
+	def model_size(self, model):
+		param_size = 0
+		buffer_size = 0
+		for param in model.parameters():
+			param_size += param.nelement() * param.element_size()
+
+		for buffer in model.buffers():
+			buffer_size += buffer.nelement() * buffer.element_size()
+
+		size_all_mb = (param_size + buffer_size) / 1024**2
+		print('Size: {:.3f} MB'.format(size_all_mb))
+
+		return size_all_mb
 
 def infer(obj_class, version):
 	n = 1
